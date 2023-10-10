a7a1297a414a435faccd44a398fbe756d011a872
Alix <alix.domange@gmail.com>
2023-10-10 10:18:40 +0200

    TP2exo2

---
+++ TP2-PHP/exo2/index.php
@@ -0,0 +1,15 @@
+<?php
+$heureCourante =  date("H:i:s");
+?>
+
+<!doctype html>
+<html>
+    <header>
+        <meta charset="utf-8">
+        <title> Heure </title>
+    </header>
+    <body>
+        <h1>Il est actuellement <?php echo $heureCourante; ?></h1>
+        
+    </body>
+</html>

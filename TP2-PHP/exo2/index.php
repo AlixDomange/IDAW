b2f66d8cfd1556a9a45ebd875e7e1828f7a90d33
Alix <alix.domange@gmail.com>
2023-10-10 10:32:47 +0200

    TP2exo2Fini

--- TP2-PHP/exo2/index.php
+++ TP2-PHP/exo2/index.php
@@ -10,6 +10,15 @@ $heureCourante =  date("H:i:s");
     </header>
     <body>
         <h1>Il est actuellement <?php echo $heureCourante; ?></h1>
-        
+        <h2><?php function afficher( $texte, $saut = 1 ) {
+                        echo $texte;
+                        for( $i = 0 ; $i < $saut ; $i++)
+                        echo "\n";
+                        }
+                        afficher("Hello", 0);
+                        afficher(" World !");?></h2>
+        <h3> <?php $tab = array(17,-3);
+                echo "tab : "; print_r($tab);?></h3>
+  
     </body>
 </html>

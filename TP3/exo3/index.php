5b9561ef5a673aef537f12cd522f7d7550891a85
Alix <alix.domange@gmail.com>
2023-10-11 11:23:25 +0200

    exo3fini

--- TP3/exo3/index.php
+++ TP3/exo3/index.php
@@ -1,17 +1,4 @@
-
-
 <!DOCTYPE html>
-
-<?php
-    if(isset($_GET['css'])) {
-        $selectedStyle = $_GET['css'];
-        setcookie("selected_style", $selectedStyle, time()+3600);
-    }
-?>
-
-<?php if(isset($_COOKIE['selected_style']))
-    echo $_COOKIE['selected_style'];
-?>
 
 <form id="style_form" action="index.php" method="GET">
     <select name="css">
@@ -21,15 +8,28 @@
     <input type="submit" value="Appliquer" />
 </form>
 
+<?php
+
+    $selectedStyle="style1";
+
+    if(isset($_COOKIE['selected_style'])){
+        $selectedStyle=$_COOKIE['selected_style'];
+    }
+
+    if(isset($_GET['css'])) {
+        $selectedStyle = $_GET['css'];
+        setcookie("selected_style", $selectedStyle, time()+3600);
+    }
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ma Page</title>
-    <?php
-    $selectedStyle = isset($_COOKIE['selected_style']) ? $_COOKIE['selected_style'] : 'style1';
-    echo'<link rel="stylesheet" href="' . $selectedStyle . '.css">';
-    ?>
+    <link rel="stylesheet" href=" <?php echo $selectedStyle ?>.css">
+
 </head>
+
 <body>
     Bienvenue
 </body>

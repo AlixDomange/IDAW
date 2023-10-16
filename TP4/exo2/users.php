16a9294cfd01e087829bd08eb27e5234ae0c1032
Alix <alix.domange@gmail.com>
2023-10-16 09:58:54 +0200

    TP4 exo 1 et 2

---
+++ TP4/exo2/users.php
@@ -0,0 +1,41 @@
+<?php
+require_once('config.php');
+
+$connectionString = "mysql:host=". _MYSQL_HOST;
+
+if(defined('_MYSQL_PORT'))
+    $connectionString .= ";port=". _MYSQL_PORT;
+
+$connectionString .= ";dbname=" . _MYSQL_DBNAME;
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
+
+$pdo = NULL;
+try {
+    $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $erreur) {
+    echo 'Erreur : '.$erreur->getMessage();
+}
+
+$request = $pdo->prepare("SELECT * FROM users");
+// TODO: add your code here
+// retrieve data from database using fetch(PDO::FETCH_OBJ) and
+// display them in HTML array
+/*** close the database connection ***/
+$request->execute();
+$users = $request->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<table>';
+echo '<tr><th>ID</th><th>Nom</th><th>Email</th></tr>';
+foreach ($users as $user) {
+    echo '<tr>';
+    echo '<td>' . $user['id'] . '</td>';
+    echo '<td>' . $user['name'] . '</td>';
+    echo '<td>' . $user['email'] . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
+
+$pdo = null;
+?>
